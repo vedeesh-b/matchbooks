fb87a2187082c2741b0e567e55b9f3f82ca49088
Vedeesh Bali <vedeeshb@Vedeeshs-MacBook-Air.local>
2026-01-05 21:43:14 +0530

    create component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useState, type FormEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import "./App.css";
 
 function App() {
   const [subject, setSubject] = useState("");
   const [books, setBooks] = useState<{ title: string }[]>([]);
+  const [queryResults, setQueryResults] = useState<{ title: string }[]>([]);
+
   const BASE_URL = "https://openlibrary.org";
 
   async function fetchBooksBySubject(subject: string) {
-    const res = await fetch(`${BASE_URL}/subjects/${subject}.json?limit=30`);
+    const res = await fetch(
+      `${BASE_URL}/search.json?q=${subject}&subject=sport`
+    );
     return res.json();
   }
+
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const keystrokes = e.target.value;
+    setSubject(keystrokes);
+    const sub = "self-help";
+    if (keystrokes.length > 2) {
+      const queryResults = await fetch(
+        `${BASE_URL}/search.json?q=${e.target.value}&subject=${sub}&limit=8`
+      );
+      const data = await queryResults.json();
+      setQueryResults(data.docs);
+    }
+  };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -28,9 +45,10 @@ function App() {
           id="subject-input"
           name="subject-input"
           value={subject}
-          onChange={(e) => setSubject(e.target.value)}
+          onChange={handleChange}
         />
         <label htmlFor="subject-input">Subject</label>
+        <div>{queryResults.map((query) => query.title)}</div>
         <button type="submit">Search</button>
       </form>
       <div>{books.map((book) => book.title)}</div>
---
+++ src/subjectMapping.ts
@@ -0,0 +1,92 @@
+export type EmotionSubjectMapping = {
+  emotion: string;
+  subjects: string[];
+};
+
+export const emotionSubjectMapping: EmotionSubjectMapping[] = [
+  {
+    emotion: "Creative",
+    subjects: ["Art", "Design", "Music", "Fashion", "Painting", "Photography"],
+  },
+  {
+    emotion: "Inspirational",
+    subjects: [
+      "Biographies",
+      "Self-help",
+      "History",
+      "Music",
+      "Entrepreneurship",
+      "Religion",
+    ],
+  },
+  {
+    emotion: "Heartwarming",
+    subjects: ["Children's", "Romance", "Family", "Young Adult"],
+  },
+  {
+    emotion: "Reflective",
+    subjects: [
+      "Biography",
+      "History",
+      "Philosophy",
+      "Poetry",
+      "Social Sciences",
+    ],
+  },
+  {
+    emotion: "Exciting",
+    subjects: ["Thriller", "Science Fiction", "Adventure", "Young Adult"],
+  },
+  {
+    emotion: "Adventurous",
+    subjects: ["Travel", "Young Adult", "Adventure", "Science Fiction"],
+  },
+  {
+    emotion: "Funny",
+    subjects: ["Humor", "Fiction", "Children's", "Graphic Design"],
+  },
+  {
+    emotion: "Magical",
+    subjects: ["Fantasy", "Fairy Tales", "Science Fiction"],
+  },
+  {
+    emotion: "Suspenseful",
+    subjects: ["Mystery", "Thriller", "Horror"],
+  },
+  {
+    emotion: "Romantic",
+    subjects: ["Romance", "Fiction", "Poetry", "Literature"],
+  },
+  {
+    emotion: "Educational",
+    subjects: ["Textbooks", "Science", "Mathematics", "History", "Psychology"],
+  },
+  {
+    emotion: "Empowering",
+    subjects: ["Self-help", "Biography", "Politics", "Business & Finance"],
+  },
+  {
+    emotion: "Therapeutic",
+    subjects: ["Mental Health", "Self-help", "Poetry"],
+  },
+  {
+    emotion: "Scary",
+    subjects: ["Horror", "Mystery", "Thriller"],
+  },
+  {
+    emotion: "Innovative",
+    subjects: ["Programming", "Technology", "Science", "Business"],
+  },
+  {
+    emotion: "Thoughtful",
+    subjects: ["Literature", "Poetry", "Philosophy", "Social Sciences"],
+  },
+  {
+    emotion: "Nostalgic",
+    subjects: ["History", "Biography", "Memoir"],
+  },
+  {
+    emotion: "Therapeutic",
+    subjects: ["Self-help", "Mental Health", "Poetry"],
+  },
+];
